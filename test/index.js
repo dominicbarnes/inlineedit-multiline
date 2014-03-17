025a337d6a59da0f334643b50ac5d1bac6630835
Dominic Barnes <dbarnes@treetopllc.com>
2014-03-17 12:15:43 -0500

    updating dominicbarnes/inlineedit and API

--- test/index.js
+++ test/index.js
@@ -4,33 +4,15 @@ var expect = require("expect.js");
 var InlineEditMultiline = require("inlineedit-multiline");
 
 describe("InlineEditMultiline(settings)", function () {
-    describe("#generateForm", function () {
-        var prop = InlineEditMultiline.prototype.generateForm;
+    describe("#generateInterface", function () {
+        var prop = InlineEditMultiline.prototype.generateInterface;
 
         it("should be a string by default", function () {
             expect(prop).to.be.a("string");
         });
 
         it("should not be the default form", function () {
-            var frag = domify(prop);
-
-            expect(frag.tagName).to.equal("FORM");
-            expect(frag.elements[0].tagName).to.equal("TEXTAREA");
-            expect(frag.elements[1].tagName).to.equal("BUTTON");
-            expect(frag.elements[2].tagName).to.equal("BUTTON");
+            expect(domify(prop).tagName).to.equal("TEXTAREA");
         });
     });
 });
-
-function createInstance(o) {
-    if (!o)         o = {};
-    if (!o.element) o.element = domify("<div>Hello World</div>");
-
-    document.body.appendChild(o.element);
-    return new InlineEdit(o);
-}
-
-function destroyInstance(instance) {
-    instance.destroy();
-    document.body.removeChild(instance.element);
-}
